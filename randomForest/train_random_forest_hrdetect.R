a0f37c0be33c2147c197971efa6e88705b1f3233
luannnguyen <36297916+luannnguyen@users.noreply.github.com>
2018-03-07 16:38:56 +0100

    Update train_random_forest_hrdetect.R

--- randomForest/train_random_forest_hrdetect.R
+++ randomForest/train_random_forest_hrdetect.R
@@ -42,6 +42,90 @@ integrateDiscrete <- function(df, x='x', y='y')
 }
 
 #========= Random forest functions =========#
+#--------- Get train/test sets ---------#
+get_kfoldTrainTest <- function(df, k=10)
+{
+   ## Total samples
+   totalSamples <- nrow(df)
+   
+   ## Number of desired TEST samples per fold
+   nSamples <- totalSamples %/% k
+   nRemainder <- totalSamples %% k
+   
+   ## Create vector of nSamples per fold (to take into account if totalSamples is not a factor of k)
+   v_nSamples <- rep(nSamples,k)
+   v_nSamples[1:nRemainder] <- v_nSamples[1:nRemainder] + 1
+   
+   ## Create list with first start/end row number for each fold
+   l_StartEnd <- list( c(1,v_nSamples[1]) ) ## initialize start/end row numbers list with first fold
+   for(i in 2:length(v_nSamples))
+   {
+      prevStartEnd <- l_StartEnd[[(i-1)]]
+      l_StartEnd[[i]] <- c(prevStartEnd[2]+1, prevStartEnd[2]+v_nSamples[i])
+   }
+   
+   ## Get train and test sets
+   l_TrainTest <- lapply(l_StartEnd, function(i)
+   {
+      Start <- i[1]
+      End <- i[2]
+      
+      testSet <- df[Start:End,]
+      trainSet <- df[-(Start:End),]
+      
+      list(train = trainSet, test = testSet)
+   })
+   
+   return(l_TrainTest)
+}
+
+## Depends on: get_kfoldTrainTest()
+get_cvTrainTestSets <- function(df, k=10, stratifyByCol = NULL)
+{
+   ## Shuffle data
+   df_shuffled <- df[sample(1:nrow(df)),]
+   
+   ## No stratification
+   if( is.null(stratifyByCol) ){
+      return( get_kfoldTrainTest(df_shuffled) )
+   }
+   
+   ## Stratification
+   else {
+      ## Split data by stratifyByCol
+      responseLvls <- df[,stratifyByCol] %>% as.factor() %>% levels()
+      l_df_strat <- lapply(responseLvls,function(lvl){ 
+         df_shuffled %>% .[.[,stratifyByCol] == lvl ,]
+      })
+      
+      ## Get train/test set per response group
+      ## List structure: l_TrainTestStrat[[responseGroup]][[kfold]][[train/test]]
+      l_TrainTestStrat <- lapply(l_df_strat, function(df_responseGroup){
+         get_kfoldTrainTest(df_responseGroup, k=k)
+      })
+      
+      ## Merge response groups per k-fold
+      nResponses <- 1:length(responseLvls)
+      
+      l_TrainTest <- lapply(1:k,function(k){
+         ## Avoided loop for train/set list levels for readability
+         trainSet <-
+            paste0('l_TrainTestStrat[[',nResponses,']][[k]][[\'train\']]', collapse=',') %>% 
+            paste0('rbind(',.,')') %>%
+            parse(text = .) %>%
+            eval()
+         testSet <-
+            paste0('l_TrainTestStrat[[',nResponses,']][[k]][[\'test\']]', collapse=',') %>% 
+            paste0('rbind(',.,')') %>%
+            parse(text = .) %>%
+            eval()
+         list(train = trainSet, test = testSet)
+      })
+      
+      return(l_TrainTest)
+   }
+}
+
 #--------- Balance classes ---------#
 balanceClasses <- function(df, colname_response, scaling = 'up')
 {
@@ -88,9 +172,6 @@ randomForest_trainAndTest <- function(df_train, df_test, colname_response, balan
                                       randomForest_ntree = 500, importance = T, ## randomForest() default args
                                       ...)
 {
-   # df_train <- l_df_train[[1]]
-   # df_test <- l_df_test[[1]]
-   
    #--------- Up/down balance classes ---------#
    if(balance == T | balance == 'up'){
       df_train <- balanceClasses(df_train, colname_response, scaling = 'up')
@@ -145,35 +226,9 @@ randomForest_trainAndTest <- function(df_train, df_test, colname_response, balan
    return(RF_CV)
 }
 
-#--------- Get train/test sets ---------#
-get_cvTrainTestSets <- function(df, k=10)
-{
-   
-   ## Shuffle data
-   df_shuffled <- df[sample(1:nrow(df)),]
-   nSamples <- round(nrow(df_shuffled)/k)
-   
-   ## Main
-   l_Train_Test <- lapply(1:k, function(i){
-      ## Get test set row indices for fold k
-      if(k != i){
-         testSet <- c( (nSamples*(i-1)+1) : (nSamples*i) )
-      } else if (k == i){
-         testSet <- c( (nSamples*(i-1)+1) : nrow(df_shuffled) ) ## Prevents error if last sample group does not have exactly nSamples.
-      }
-      
-      ## Return list of train and test sets
-      list(train = df_shuffled[-testSet,],
-           test = df_shuffled[testSet,]
-      )
-   })
-   
-   return(l_Train_Test)
-}
-
 #--------- Random forest k-fold cross validation ---------#
 ## Depends on: get_cvTrainTestSets()
-randomForest_CV <- function(df,colname_response,k=10,...)
+randomForestCV <- function(df,colname_response,k=10,...)
 {
    l_TrainTestSets <- get_cvTrainTestSets(df,k=k)
    
@@ -305,7 +360,7 @@ plot_tfpnRates <- function(v_prob_predicted=NULL, v_logical_expected=NULL, df_me
    if(showIntersection == T){
       
       diff_tpnRate <- abs( df_melt[df_melt$metric == 'tpr','y'] - df_melt[df_melt$metric == 'tnr','y'] )
-      intersection <- which(diff_tpnRate == min(diff_tpnRate)) %>% df_melt[.,'x']
+      intersection <- which(diff_tpnRate == min(diff_tpnRate)) %>% df_melt[.,'x'] %>% signif(.,4)
       
       tfpnRatesPlot <- tfpnRatesPlot +
          geom_vline(xintercept = intersection,linetype = 3,colour = 'black') +
@@ -342,7 +397,7 @@ plot_ROC <- function(v_prob_predicted=NULL, v_logical_expected=NULL, title='ROC'
    }
    
    if(showAUC == T){
-      auc <- prediction(v_prob_predicted, v_logical_expected) %>% performance(.,'auc') %>% .@y.values %>% unlist()
+      auc <- prediction(v_prob_predicted, v_logical_expected) %>% performance(.,'auc') %>% .@y.values %>% unlist() 
       ROCPlot <- ROCPlot + annotate('text', x=0.5, y=min(df_tpr_fpr$y), hjust = 0.5, vjust = 0.5, label=paste0('AUROC = ', auc %>% round(.,4)))
    }
    
@@ -352,7 +407,8 @@ plot_ROC <- function(v_prob_predicted=NULL, v_logical_expected=NULL, title='ROC'
 #--------- Plot precision recall ---------#
 ## Precision/Positive predictive value: probability that subjects with a positive screening test truly have the disease
 ## Recall/True positive rate
-calc_AUPRC <- function(v_prob_predicted, v_logical_expected){
+calc_AUPRC <- function(v_prob_predicted, v_logical_expected)
+{
    df_melt <- perfAsDf(v_prob_predicted, v_logical_expected, metrics=c('ppv','tpr'))
    
    ## Prepare dataframe
@@ -452,7 +508,7 @@ plot_AUCFeatSel <- function(df_summaryFeatSel, metric='auprc')
 
 #========= Paths =========#
 localPathPrefix <- '/Users/lnguyen/'
-hpcPath <- '/hpc/cog_bioinf/cuppen/project_data/Luan_PCAGW/results/hr_detect/random_forest_training/trainingMain_umcuData_whitelist/'
+hpcPath <- '/hpc/cog_bioinf/cuppen/project_data/Luan_PCAGW/results/hr_detect/random_forest_training/feature_selection_umcuData_whitelist_contextMatrix/'
 if( dir.exists(localPathPrefix) ){
    base_dir <- paste0(localPathPrefix, hpcPath)
 } else {
@@ -464,14 +520,25 @@ if( dir.exists(localPathPrefix) ){
 donorWhitelist <- read.table( '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_PCAGW/results/hr_detect/ML_data/donorWhitelist.tsv', sep='\t',header=T)
 donorWhitelistNames <- rownames(donorWhitelist)
 
-#========= UMCU data =========#
+#========= Input: UMCU data =========#
+## Norm signature data
 df_umcuNormData <- read.table( paste0(base_dir,'df_umcuNormData_BRCAsimple.tsv'),sep='\t',header=T)
 df_umcuNormData$response <- as.factor(df_umcuNormData$response)
 
 df_umcuNormData_whitelist <- df_umcuNormData %>% .[rownames(.) %in% donorWhitelistNames,]
 
+## Context data
+df_umcuDataContext <- read.table('/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_PCAGW/results/hr_detect/ML_data/df_umcuDataContext_BRCAannot.tsv',sep='\t',header=T,check.names=F)
+
+df_umcuDataContext$response <- df_umcuDataContext$response %>% get_simpleResponse(.,c('BRCA1','BRCA2'),'BRCA','none','none')
+df_umcuDataContext_whitelist <- df_umcuDataContext %>% .[rownames(.) %in% donorWhitelistNames,]
+
+#========= Set input =========#
+#df <- df_umcuNormData_whitelist
+df <- df_umcuDataContext_whitelist
+
 #========= CV and plots =========#
-rfCV <- randomForest_CV(df_umcuNormData_whitelist, colname_response = 'response',inclExpectedResponse = T)
+rfCV <- randomForestCV(df, colname_response = 'response', inclExpectedResponse = T)
 
 ## Aggregate information
 agg_pred <- aggregate_rfCV(rfCV, 'prediction')
@@ -481,7 +548,7 @@ agg_imp <- aggregate_rfCV(rfCV, 'importance')
 
 ## Plotting
 rfCV_summaryPlots <- list(
-   plot_MeanDecreaseAccuracy(agg_imp,topn = F),
+   plot_MeanDecreaseAccuracy(agg_imp,topn = 50),
    plot_tfpnRates(agg_pred$BRCA,agg_pred$response),
    plot_ROC(agg_pred$BRCA,agg_pred$response),
    plot_PRC(agg_pred$BRCA,agg_pred$response)
@@ -493,9 +560,7 @@ grid.arrange(grobs = rfCV_summaryPlots, nrow = 2, ncol = 2)
 dev.off()
 
 #========= Feature selection =========#
-df <- df_umcuNormData_whitelist
-
-nFeatures <- ncol(df_umcuNormData_whitelist)-1
+nFeatures <- ncol(df)-1
 featSelIters <- 1:(nFeatures-1) ## End when only 1 feature(s) are left
 
 f_report <- paste0(base_dir,'summaryFeatureSelection.tsv')
@@ -507,8 +572,7 @@ for(iter in featSelIters){
    iterName <- str_pad(iter, 2, pad = "0")
    
    df_ss <- df[,!(colnames(df) %in% names(v_minImpVar))]
-   rfCV <- randomForest_CV(df_ss, 
-                           colname_response = 'response',inclExpectedResponse = T)
+   rfCV <- randomForestCV(df_ss, colname_response = 'response', inclExpectedResponse = T)
    
    ## Aggregate information
    message('Aggregating CV information...')
